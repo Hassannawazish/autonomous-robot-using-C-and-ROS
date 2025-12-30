ee555a6f28ee3b0021277cc3a6d15a805da6052a
Hassan Nawazish <50699910+Hassannawazish@users.noreply.github.com>
2025-12-30 16:48:51 +0100

    shell scripts for simulation

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install ros-humble-turtlebot3*
+sudo apt install ros-humble-gazebo-ros-pkgs
+export TURTLEBOT3_MODEL=burger
+echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
+source ~/.bashrc
---
+++ simulation.sh
@@ -0,0 +1,3 @@
+cd ~
+export TURTLEBOT3_MODEL=burger
+ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py
